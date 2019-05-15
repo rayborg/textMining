3028a019a97d6611704c9251ee051f7f28055aee
Raymond C. Borges Hink <borgesraymond@gmail.com>
2019-05-15 19:53:10 -0400

    Takes PDF and summarizes it

---
+++ PDFsummarizer.sh
@@ -0,0 +1,38 @@
+#Requirements
+#sudo apt install python-pdfminer (https://pypi.org/project/pdfminer/)
+#pip install summa  (https://pypi.org/project/summa/)
+
+#Takes a directory as input
+#!/bin/bash
+
+for f in *.pdf
+do
+  echo "Processing $f file..."
+  
+  #Converts PDF to text
+  pdf2txt "$f" > tempFile.txt
+ 
+  echo "" >> output.txt
+  echo "" >> output.txt
+  echo "*TITLE:$f**" >> output.txt
+  
+  #Sanitizing file
+  #Removes non-alpha characters
+  sed -i tempFile.txt -e "s/[^ a-zA-Z']//g" -e 's/ \+/ /' 
+  
+  #Removing duplicate lines caused for a number of reasons
+  cat tempFile.txt | sort | uniq > tempFile.txt
+  
+  #Summarizes the output and appends to output.txt
+  textrank -t tempFile.txt -r .01 >> output.txt
+
+done
+
+
+
+
+
+
+
+
+
